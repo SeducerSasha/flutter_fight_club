36b1d45b1dfc3a5e5806edd6eb585bf41a882f03
SeducerSasha <seducer@bk.ru>
2023-11-12 22:14:01 +0300

    Commit finish-2

--- lib/pages/statistics_page.dart
+++ lib/pages/statistics_page.dart
@@ -3,14 +3,9 @@ import 'package:flutter_fight_club/resources/fight_club_colors.dart';
 import 'package:flutter_fight_club/widgets/secondary_action_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class StatisticPage extends StatefulWidget {
+class StatisticPage extends StatelessWidget {
   const StatisticPage({super.key});
 
-  @override
-  State<StatisticPage> createState() => _StatisticPageState();
-}
-
-class _StatisticPageState extends State<StatisticPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,43 +18,69 @@ class _StatisticPageState extends State<StatisticPage> {
             child: const Text('Statistics'),
           ),
           Expanded(
-              child: Center(
-            child: SizedBox(
-              height: 132,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  FutureBuilder<int?>(
-                      future: SharedPreferences.getInstance().then(
-                          (sharedPrefs) => sharedPrefs.getInt('stats_won')),
-                      builder: (context, snapshot) {
-                        if (!snapshot.hasData || snapshot.data == null) {
-                          return const Text('Won: 0');
-                        }
-                        return Text('Won: ${snapshot.data}');
-                      }),
-                  FutureBuilder<int?>(
-                      future: SharedPreferences.getInstance().then(
-                          (sharedPrefs) => sharedPrefs.getInt('stats_draw')),
-                      builder: (context, snapshot) {
-                        if (!snapshot.hasData || snapshot.data == null) {
-                          return const Text('Draw: 0');
-                        }
-                        return Text('Draw: ${snapshot.data}');
-                      }),
-                  FutureBuilder<int?>(
-                      future: SharedPreferences.getInstance().then(
-                          (sharedPrefs) => sharedPrefs.getInt('stats_lost')),
-                      builder: (context, snapshot) {
-                        if (!snapshot.hasData || snapshot.data == null) {
-                          return const Text('Lost: 0');
-                        }
-                        return Text('Lost: ${snapshot.data}');
-                      }),
-                ],
+            child: Center(
+              child: SizedBox(
+                height: 132,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: FutureBuilder<int?>(
+                          future: SharedPreferences.getInstance().then(
+                              (sharedPrefs) => sharedPrefs.getInt('stats_won')),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData || snapshot.data == null) {
+                              return const Text(
+                                'Won: 0',
+                                style: TextStyle(fontSize: 16),
+                              );
+                            }
+                            return Text(
+                              'Won: ${snapshot.data}',
+                              style: TextStyle(fontSize: 16),
+                            );
+                          }),
+                    ),
+                    Expanded(
+                      child: FutureBuilder<int?>(
+                          future: SharedPreferences.getInstance().then(
+                              (sharedPrefs) =>
+                                  sharedPrefs.getInt('stats_draw')),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData || snapshot.data == null) {
+                              return const Text(
+                                'Draw: 0',
+                                style: TextStyle(fontSize: 16),
+                              );
+                            }
+                            return Text(
+                              'Draw: ${snapshot.data}',
+                              style: TextStyle(fontSize: 16),
+                            );
+                          }),
+                    ),
+                    Expanded(
+                      child: FutureBuilder<int?>(
+                          future: SharedPreferences.getInstance().then(
+                              (sharedPrefs) =>
+                                  sharedPrefs.getInt('stats_lost')),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData || snapshot.data == null) {
+                              return const Text(
+                                'Lost: 0',
+                                style: TextStyle(fontSize: 16),
+                              );
+                            }
+                            return Text(
+                              'Lost: ${snapshot.data}',
+                              style: TextStyle(fontSize: 16),
+                            );
+                          }),
+                    ),
+                  ],
+                ),
               ),
             ),
-          )),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16),
             child: SecondaryActionButton(
